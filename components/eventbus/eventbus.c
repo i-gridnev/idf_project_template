36960597f25aa1616520a3d7e10dbe4418e61a8d
icer_dev <mr.igridnev@gmail.com>
2025-11-16 17:28:15 +0000

    recreation of core

--- components/eventbus/eventbus.c
+++ components/eventbus/eventbus.c
@@ -10,8 +10,6 @@
 #define EVENT_TASK_CORE  1
 
 typedef struct {
-    module_base** registry;
-    size_t modules_amount;
     QueueHandle_t event_queue;
     TaskHandle_t event_task;
 } eventbus_t;
@@ -23,40 +21,61 @@ static eventbus_t EVENTBUS;
 //===============================================================================//
 //================================= Private =====================================//
 //===============================================================================//
-esp_err_t
-module_create(module_base* self, module_base_config_t* config) {
-    self->id = config->id;
-    self->size = config->max_evts;
-    self->event_handler = config->event_handler;
+// esp_err_t
+// module_create(module_base* self, module_base_config_t* config) {
+//     self->id = config->id;
+//     self->size = config->max_evts;
+//     self->event_handler = config->event_handler;
 
-    self->subscriptions = malloc(self->size * sizeof(struct subscription_head));
-    if (!self->subscriptions) {
-        return ESP_ERR_NO_MEM;
-    }
-    for (int i = 0; i < self->size; i++) {
-        SLIST_INIT(&self->subscriptions[i]);
-    };
-    return ESP_OK;
-}
+//     self->subscriptions = malloc(self->size * sizeof(struct subscription_head));
+//     if (!self->subscriptions) {
+//         return ESP_ERR_NO_MEM;
+//     }
+//     for (int i = 0; i < self->size; i++) {
+//         SLIST_INIT(&self->subscriptions[i]);
+//     };
+//     return ESP_OK;
+// }
 
 static void
 eventbus_task(void* params) {
+    esp_err_t err = ESP_OK;
     event_t event;
 
     while (true) {
         if (xQueueReceive(EVENTBUS.event_queue, &event, 1)) {
-            subscription_t* sub;
-            SLIST_FOREACH(sub, &event.issuer->subscriptions[event.id], next) {
-                if (sub->module->event_handler != NULL) {
-                    esp_err_t err = sub->module->event_handler(sub->module, &event);
-                    if (err != ESP_OK) {
-                        ESP_LOGW(TAG, "event id=%d issuer=%d handling err=%d(%s)", event.id, event.issuer->id, err,
-                                 esp_err_to_name(err));
-                    }
+            middleware_list_t* middleware;
+            SLIST_FOREACH(middleware, event.issuer->module_ptr->middlewares, next) {
+                err = middleware->handler(&event);
+                if (err != ESP_OK) {
+                    ESP_LOGW(TAG, "event id=%d issuer %s (instance_id=%d) middleware err=%d(%s)", event.id,
+                             event.issuer->module_ptr->name, event.issuer->instance_id, err, esp_err_to_name(err));
                 }
             }
-            if (event.payload.free_fcn) {
-                event.payload.free_fcn(event.payload.data.ptr);
+
+            bool sunscriptions_found = false;
+            event_list_t* event_list;
+            SLIST_FOREACH(event_list, event.issuer->event_list, next) {
+                if (event_list->event_id == event.id) {
+                    subscription_t* sub;
+                    sunscriptions_found = true;
+                    SLIST_FOREACH(sub, event_list->subscriptions, next) {
+                        err = sub->handler(sub->subscriber, &event);
+                        if (err != ESP_OK) {
+                            ESP_LOGW(TAG, "event id=%d issuer %s (instance_id=%d) handling err=%d(%s)", event.id,
+                                     event.issuer->module_ptr->name, event.issuer->instance_id, err,
+                                     esp_err_to_name(err));
+                        }
+                    }
+                    break;
+                }
+            }
+            if (!sunscriptions_found) {
+                ESP_LOGW(TAG, "event id=%d issuer %s (instance_id=%d) no subscribers", event.id,
+                         event.issuer->module_ptr->name, event.issuer->instance_id);
+            }
+            if (event.data_free_fcn) {
+                event.data_free_fcn(event.data);
             }
         }
     }
@@ -67,13 +86,8 @@ eventbus_task(void* params) {
 //===============================================================================//
 
 esp_err_t
-eventbus_init(size_t modules_amount) {
-    EVENTBUS.modules_amount = modules_amount;
+eventbus_init() {
     ESP_ERROR_CHECK(esp_event_loop_create_default());
-    EVENTBUS.registry = calloc(EVENTBUS.modules_amount, sizeof(module_base*));
-    if (!EVENTBUS.registry) {
-        return ESP_ERR_NO_MEM;
-    }
     EVENTBUS.event_queue = xQueueCreateStatic(EVENT_QUEUE_SIZE, sizeof(event_t), __eq_buf, &__eq_struct);
     if (!EVENTBUS.event_queue) {
         return ESP_ERR_NO_MEM;
@@ -87,57 +101,160 @@ eventbus_init(size_t modules_amount) {
     return ESP_OK;
 }
 
-esp_err_t
-eventbus_module_register(module_base* module, module_base_config_t* config) {
-    esp_err_t err = ESP_OK;
-    if (config->id < 0 || config->id > EVENTBUS.modules_amount) {
-        ESP_LOGE(TAG, "unknown id %d", config->id);
-        err = ESP_FAIL;
-    } else if (EVENTBUS.registry[config->id]) {
-        ESP_LOGE(TAG, "already exist id %d", config->id);
-        err = ESP_ERR_INVALID_ARG;
-    } else {
-        err = module_create(module, config);
-        if (err == ESP_OK) {
-            EVENTBUS.registry[module->id] = module;
+// esp_err_t
+// eventbus_module_register(module_base* module, module_base_config_t* config) {
+//     esp_err_t err = ESP_OK;
+//     if (config->id < 0 || config->id > EVENTBUS.modules_amount) {
+//         ESP_LOGE(TAG, "unknown id %d", config->id);
+//         err = ESP_FAIL;
+//     } else if (EVENTBUS.registry[config->id]) {
+//         ESP_LOGE(TAG, "already exist id %d", config->id);
+//         err = ESP_ERR_INVALID_ARG;
+//     } else {
+//         err = module_create(module, config);
+//         if (err == ESP_OK) {
+//             EVENTBUS.registry[module->id] = module;
+//         }
+//     }
+//     return err;
+// }
+
+// module_base*
+// eventbus_module_get(int id) {
+//     if (id < 0 || id > EVENTBUS.modules_amount) {
+//         ESP_LOGE(TAG, "unknown id %d", id);
+//         return NULL;
+//     }
+//     return EVENTBUS.registry[id];
+// }
+
+// esp_err_t
+// eventbus_module_subscribe(module_base* self, int target_id, int evt_id) {
+//     module_base* addressee = eventbus_module_get(target_id);
+//     if (!addressee) {
+//         ESP_LOGE(TAG, "sub addressee not found for id %d", target_id);
+//         return ESP_FAIL;
+//     }
+//     if (evt_id > addressee->size) {
+//         ESP_LOGE(TAG, "unknown sub event id %d", evt_id);
+//         return ESP_FAIL;
+//     }
+//     subscription_t* new_sub = malloc(sizeof(subscription_t));
+//     if (!new_sub) {
+//         return ESP_ERR_NO_MEM;
+//     }
+//     new_sub->module = self;
+//     SLIST_INSERT_HEAD(&addressee->subscriptions[evt_id], new_sub, next);
+//     return ESP_OK;
+// }
+
+void
+eventbus_module_constructor(module_base_t* module, char* name) {
+    module->name = name;
+    SLIST_INIT(module->middlewares);
+    SLIST_INIT(module->instances);
+}
+
+instance_base_t*
+eventbus_module_get_instance(module_base_t* module, int id) {
+    instance_base_t* base = NULL;
+    instance_list_t* instance_list_item;
+    SLIST_FOREACH(instance_list_item, module->instances, next) {
+        if (instance_list_item->instance->instance_id == id) {
+            base = instance_list_item->instance;
+            break;
         }
     }
+    return base;
+}
+
+esp_err_t
+eventbus_module_add_middleware(module_base_t* module, middleware_handler handler) {
+    esp_err_t err = ESP_OK;
+    middleware_list_t* last_item = NULL;
+    middleware_list_t* item;
+
+    SLIST_FOREACH(item, module->middlewares, next) {
+        if (item->handler == handler) {
+            err = ESP_ERR_NOT_ALLOWED;
+            ESP_LOGE(TAG, "failed add existing middleware for %s", module->name);
+            return err;
+        }
+        if (SLIST_NEXT(item, next) == NULL) {
+            last_item = item;
+        }
+    }
+
+    middleware_list_t* new_list_item = calloc(1, sizeof(middleware_list_t));
+    new_list_item->handler = handler;
+    SLIST_INSERT_AFTER(last_item, new_list_item, next);
     return err;
 }
 
-module_base*
-eventbus_module_get(int id) {
-    if (id < 0 || id > EVENTBUS.modules_amount) {
-        ESP_LOGE(TAG, "unknown id %d", id);
-        return NULL;
+esp_err_t
+eventbus_module_add_instance(module_base_t* module, instance_base_t* instance) {
+    esp_err_t err = ESP_OK;
+    instance_list_t* last_item = NULL;
+    instance_list_t* item;
+
+    SLIST_FOREACH(item, module->instances, next) {
+        if (item->instance == instance) {
+            err = ESP_ERR_NOT_ALLOWED;
+            ESP_LOGE(TAG, "instance_id=%d (%s) already exist", instance->instance_id, module->name);
+            return err;
+        }
+        if (SLIST_NEXT(item, next) == NULL) {
+            last_item = item;
+        }
     }
-    return EVENTBUS.registry[id];
+
+    instance_list_t* new_list_item = calloc(1, sizeof(instance_list_t));
+    new_list_item->instance = instance;
+    SLIST_INSERT_AFTER(last_item, new_list_item, next);
+    return err;
 }
 
 esp_err_t
-eventbus_module_subscribe(module_base* self, int target_id, int evt_id) {
-    module_base* addressee = eventbus_module_get(target_id);
-    if (!addressee) {
-        ESP_LOGE(TAG, "sub addressee not found for id %d", target_id);
-        return ESP_FAIL;
+eventbus_subscribe(instance_base_t* self, instance_base_t* target, int event_id, event_handler handler) {
+    esp_err_t err = ESP_OK;
+    bool list_item_found = false;
+
+    event_list_t* event_list_item;
+
+    SLIST_FOREACH(event_list_item, target->event_list, next) {
+        if (event_list_item->event_id == event_id) {
+            list_item_found = true;
+            //__insert
+            subscription_t* new_sub_item = calloc(1, sizeof(subscription_t));
+            new_sub_item->subscriber = self;
+            new_sub_item->handler = handler;
+
+            subscription_t* last_sub_item = NULL;
+            subscription_t* sub_item;
+            SLIST_FOREACH(sub_item, event_list_item->subscriptions, next) {
+                if (SLIST_NEXT(sub_item, next) == NULL) {
+                    last_sub_item = sub_item;
+                }
+            }
+            if (last_sub_item) {
+                SLIST_INSERT_AFTER(last_sub_item, new_sub_item, next);
+            }
+            break;
+        }
     }
-    if (evt_id > addressee->size) {
-        ESP_LOGE(TAG, "unknown sub event id %d", evt_id);
-        return ESP_FAIL;
+    if (!list_item_found) {
+        event_list_t* new_event_list_item = calloc(1, sizeof(event_list_t));
+        new_event_list_item->event_id = event_id;
+        SLIST_INSERT_HEAD(target->event_list, new_event_list_item, next);
+        SLIST_INSERT_AFTER(new_event_list_item, new_sub_item, next);
+        //__insert
     }
-    subscription_t* new_sub = malloc(sizeof(subscription_t));
-    if (!new_sub) {
-        return ESP_ERR_NO_MEM;
-    }
-    new_sub->module = self;
-    SLIST_INSERT_HEAD(&addressee->subscriptions[evt_id], new_sub, next);
-    return ESP_OK;
 }
 
 esp_err_t
 eventbus_post_event(event_t* event) {
     if (!xQueueSend(EVENTBUS.event_queue, event, 0)) {
-        ESP_LOGE(TAG, "failed to post evt: id=%d, issuer_id=%d", event->id, event->issuer->id);
+        ESP_LOGE(TAG, "failed to post evt: id=%d, issuer_id=%d", event->id, event->issuer->instance_id);
         return ESP_FAIL;
     }
     return ESP_OK;
--- components/eventbus/include/eventbus.h
+++ components/eventbus/include/eventbus.h
@@ -12,64 +12,74 @@
 #include <sys/queue.h>
 #include "esp_err.h"
 
-#define ANY_MODULE_ID -1
+typedef struct instance_base instance_base_t;
+typedef struct module_base module_base_t;
 
-typedef struct event event_t;
-typedef struct module_base module_base;
-typedef esp_err_t (*event_handler)(module_base* self, event_t* event);
+typedef struct {
+    instance_base_t* issuer;
+    int id;
+    void* data;
+    size_t data_size;
+    void (*data_free_fcn)(void* data);
+} event_t;
+
+typedef esp_err_t (*event_handler)(instance_base_t* subscriber, event_t* event);
+
+typedef esp_err_t (*middleware_handler)(event_t* event);
 
 typedef struct subscription {
-    module_base* module;
+    instance_base_t* subscriber;
+    event_handler handler;
     SLIST_ENTRY(subscription) next;
 } subscription_t;
 
 SLIST_HEAD(subscription_head, subscription);
 
-struct module_base {
-    int id;
-    size_t size;
-    event_handler event_handler;
+typedef struct event_list {
+    int event_id;
     struct subscription_head* subscriptions;
+    SLIST_ENTRY(event_list) next;
+} event_list_t;
+
+SLIST_HEAD(event_list_head, event_list);
+
+typedef struct instance_list {
+    instance_base_t* instance;
+    SLIST_ENTRY(instance_list) next;
+} instance_list_t;
+
+SLIST_HEAD(instance_list_head, instance_list);
+
+typedef struct middleware_list {
+    middleware_handler handler;
+    SLIST_ENTRY(middleware_list) next;
+} middleware_list_t;
+
+SLIST_HEAD(middleware_list_head, middleware_list);
+
+struct instance_base {
+    module_base_t* module_ptr;
+    int instance_id;
+    struct event_list_head* event_list;
 };
 
-typedef struct {
-    int id;
-    size_t max_evts;
-    event_handler event_handler;
-} module_base_config_t;
+struct module_base {
+    char* name;
+    struct middleware_list_head* middlewares;
+    struct instance_list_head* instances;
+};
 
-typedef struct event {
-    int id;
-    module_base* issuer;
+esp_err_t eventbus_init();
 
-    struct {
-        union sruct {
-            void* ptr;
-            char* str;
-            int i32;
-            uint16_t u16;
-            float f32;
-            bool b;
+// esp_err_t eventbus_module_register(module_base* module, module_base_config_t* config);
 
-            struct {
-                uint16_t u1;
-                uint16_t u2;
-            } double_u16;
-        } data;
+void eventbus_module_constructor(module_base_t* module, char* name);
 
-        size_t size;
-        void (*free_fcn)(void* data);
-    } payload;
-} event_t;
-
-esp_err_t eventbus_init(size_t modules_amount);
-
-esp_err_t eventbus_module_register(module_base* module, module_base_config_t* config);
-
-module_base* eventbus_module_get(int id);
-
-esp_err_t eventbus_module_subscribe(module_base* self, int target_id, int evt_id);
+esp_err_t eventbus_module_add_middleware(module_base_t* module, middleware_handler handler);
+esp_err_t eventbus_module_add_instance(module_base_t* module, instance_base_t* instance);
+instance_base_t* eventbus_module_get_instance(module_base_t* module, int id);
 
 esp_err_t eventbus_post_event(event_t* event);
+esp_err_t eventbus_subscribe(instance_base_t* self, instance_base_t* target, int event_id, event_handler handler);
 
 #endif /* _EVENTBUS_H_ */
